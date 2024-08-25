9330c895a79305db0a72ec375901d1eab9ee7285
YaroslavKSE <ikhomych@kse.org.ua>
2024-08-25 11:26:35 +0300

    Added mongo dockerfile, removed hardcoded env variables.

--- internship_project/postgres-init/import-dump.sh
+++ internship_project/postgres-init/import-dump.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 set -e
 
-# Function to check if the dump has already been imported
 dump_imported() {
     psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -c "SELECT 1 FROM pg_tables WHERE tablename = 'databasechangelog'" | grep -q 1
 }
 
-# Check if the database exists
 database_exists=$(psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --tuples-only -c "SELECT 1 FROM pg_database WHERE datname = '$POSTGRES_DB'")
 
 if [ -z "$database_exists" ]; then
@@ -14,7 +12,6 @@ if [ -z "$database_exists" ]; then
     createdb -U "$POSTGRES_USER" "$POSTGRES_DB"
 fi
 
-# Import the dump if it hasn't been imported yet
 if ! dump_imported; then
     echo "Importing database dump"
     psql -v ON_ERROR_STOP=0 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" < /docker-entrypoint-initdb.d/database.sql
